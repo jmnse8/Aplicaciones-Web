5a65cdfffe58085e867bf16373a7841dd4d45b77
RempalagoMcWilson <72865314+RempalagoMcWilson@users.noreply.github.com>
2022-10-09 21:26:18 +0200

    avanzado print

---
+++ Prácticas voluntarias/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let listaTareas = [
+    {text:"Preparar prácticas AW", tags:["universidad","awt"]}
+    , {text: "Mirar fechas congreso", done: true, tags:[]}
+    , {text: "Ir al supermercado", tags: ["personal", "básico"]}
+    , {text: "Jugar al fútbol", done: false, tags:["personal", "deportes"]}
+    , {text: "Hablar con profesor", done: false, tags:["universidad", "tp2"]}
+    ];
+    
