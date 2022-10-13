fe0689b2c18f9119d1a97dce70e67132df16bb20
RempalagoMcWilson <72865314+RempalagoMcWilson@users.noreply.github.com>
2022-10-13 19:07:45 +0200

    Update script.js

--- Prácticas voluntarias/script.js
+++ Prácticas voluntarias/script.js
@@ -7,9 +7,32 @@ let listaTareas = [
     , {text: "Jugar al fútbol", done: false, tags:["personal", "deportes"]}
     , {text: "Hablar con profesor", done: false, tags:["universidad", "tp2"]}
     ];
-    
+/*
 function getToDoTasks(tasks) {
     return (!(tasks instanceof Array)) ? [] : tasks.filter(t => (t["done"] == undefined || !t["done"])).map(e => e["text"]);
 }
 
-getToDoTasks(listaTareas);
+function findByTag(tasks, tag) {
+    return (!(tasks instanceof Array)) ? [] : tasks.filter(t => (t.tags.includes(tag)));
+}
+
+function findByTags(tasks, tags) {/Falta por hacer
+    return (!(tasks instanceof Array) || !tags instanceof Array) ? [] 
+        : tasks.filter(t => (t.tags.includes(tags)));
+}
+
+function countDone(tasks) {
+    return (!(tasks instanceof Array)) ? 0 : tasks.filter(t => t["done"]).length;
+}
+*/
+
+function createTask(texto) {
+
+}
+
+//countDone(listaTareas);
+
+//findByTags(listaTareas, ["personal","practica"]);
+
+//getToDoTasks(listaTareas);
+//findByTag(listaTareas, "personal");
